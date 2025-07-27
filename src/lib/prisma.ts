3c853b6b6d21b218184f657fe78392c532432e1f
Febrian Nugraha <febrian.14nugraha@gmail.com>
2025-07-27 22:23:42 +0700

    02: database auth setup

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,26 @@
 // lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+// Cek apakah 'prisma' sudah ada di global, jika tidak, baru buat instance baru.
+// Pembuatan adapter terjadi di sini, jadi hanya berjalan sekali.
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaNeon({
+      connectionString: `${process.env.DATABASE_URL}`,
+    }),
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// Simpan instance ke global hanya di lingkungan development
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
